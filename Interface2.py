f498cc69a150b818eb8e57ca1f5f6844344e80df
Hitanshee <75136823+Hitanshee@users.noreply.github.com>
2023-04-23 11:36:40 +0530

    Update Interface2.py

--- Interface2.py
+++ Interface2.py
@@ -8,9 +8,9 @@ generator = None
 def load_model():
     global generator
     global processor, model
-    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
-    generator = pipeline('text-generation', model='gpt2')
+    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large") #pre-trained blip model from huggingface
+    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large") #pre-trained blip model from huggingface
+    generator = pipeline('text-generation', model='gpt2') #pre-trained gpt2 from huggingface
 
 @app.before_first_request
 def before_first_request():
