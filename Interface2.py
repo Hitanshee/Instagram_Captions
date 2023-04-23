3e5e06ea178c78a518636e94cfdf645664d5cbe7
Hitanshee <75136823+Hitanshee@users.noreply.github.com>
2023-04-23 18:18:40 +0530

    Updated

--- Interface2.py
+++ Interface2.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from transformers import pipeline, BlipProcessor, BlipForConditionalGeneration
 from PIL import Image
+import base64
 
 app = Flask(__name__)
 generator = None
@@ -8,9 +9,9 @@ generator = None
 def load_model():
     global generator
     global processor, model
-    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large") #pre-trained blip model from huggingface
-    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large") #pre-trained blip model from huggingface
-    generator = pipeline('text-generation', model='gpt2') #pre-trained gpt2 from huggingface
+    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+    generator = pipeline('text-generation', model='gpt2')
 
 @app.before_first_request
 def before_first_request():
@@ -21,7 +22,7 @@ def before_first_request():
 def index():
     return render_template('input.html')
 
-def caption_gen(img_path): #text-generation
+def caption_gen(img_path):
     raw_image = Image.open(img_path).convert('RGB')
     inputs = processor(raw_image, return_tensors="pt")
     out = model.generate(**inputs)
@@ -29,8 +30,10 @@ def caption_gen(img_path): #text-generation
 
 @app.route('/generate', methods=['POST'])
 
-def generate_captions(): #image-to-text
+def generate_captions():
     image_path = request.files['image_file']
+    file_bytes = image_path.read()
+    file_base64 = base64.b64encode(file_bytes).decode('ascii')
     image_description = caption_gen(image_path)
     num_captions = int(request.form['num_captions'])
     captions = []
@@ -38,7 +41,7 @@ def generate_captions(): #image-to-text
         caption = generator(f"{image_description}\n", max_length=50, do_sample=True, temperature=0.7)[0]['generated_text']
         #captions.append(caption)
         captions.append(caption[len(image_description)+1:])
-    return render_template('output.html', captions=captions)
+    return render_template('output.html', image_base64=file_base64, output=image_description,  captions=captions)
 
 if __name__ == '__main__':
     app.run(debug=True)
